64a2d03b2d371133b25b2a38ed4a1a6ed9433a3d
Diego Carvajal <diego.carvajal@usco.edu.coclear>
2024-09-22 18:47:04 -0500

    Initial Commit

---
+++ src/main/java/co/edu/pw/FirstServlet.java
@@ -0,0 +1,60 @@
+package co.edu.pw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FirstServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			String n = request.getParameter("userName");
+			out.print("Welcome " + n);
+			out.print("<br/>");
+
+			// appending the username in the query string
+			out.print("<a href='servlet2?uname=" + n + "'>visit</a>");
+
+			out.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/co/edu/pw/SecondServlet.java
@@ -0,0 +1,57 @@
+package co.edu.pw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public SecondServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			// getting value from the query string
+			String n = request.getParameter("uname");
+			out.print("Hello " + n);
+
+			out.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
